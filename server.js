4d91201e81bc344eba6612d1637e4f8f017b268e
Anish jha <122715402+anish-stack@users.noreply.github.com>
2024-01-10 14:15:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,29 +4,23 @@ const dotenv = require('dotenv');
 dotenv.config();
 const Port = process.env.PORT || 4000;
 const ConnectDB = require('./database/db');
+
 const cors = require('cors');
 const UserRoutes = require('./routes/userroutes');
 const cookiesParser = require('cookie-parser')
 
 ConnectDB();
+const corsOption = {
+  origin: 'https://seone-g7g.com',
+  optionSuccessStatus:200
+}
 
 // Middleware
-app.use(cors());
+app.use(cors(corsOption));
 app.use(cookiesParser())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const errorHandler = (err, req, res, next) => {
-  console.error('Unhandled error:', err);
 
-  // Customize the error response based on your application's needs
-  res.status(500).json({
-    success: false,
-    message: 'Internal Server Error',
-  });
-};
-
-// Add the error handler middleware to the end of the middleware stack
-app.use(errorHandler);
 // Routes
 app.use('/api/v1', UserRoutes);
 app.use('/', (req,res)=>{
